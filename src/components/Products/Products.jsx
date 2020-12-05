9688ef6ebc660b8b355150cc1efa28faa828a1c0
CJ Celimen <cjdcelimen@gmail.com>
2020-12-05 08:11:04 +0800

    Update products using Commerce.js

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,12 +3,8 @@ import Grid from '@material-ui/core/Grid';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const Products = () => {
+const Products = ({ products }) => {
     const classes = useStyles();
-    const products = [
-        { id: 1, name: 'Ultra Boost', description: 'adidas Ultra Boost S&L Star Wars X-Wing', price: '$100', image: 'https://stockx-360.imgix.net//adidas-Ultra-Boost-SL-Star-Wars-X-Wing/Images/adidas-Ultra-Boost-SL-Star-Wars-X-Wing/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1603481985&w=1000' },
-        { id: 2, name: 'MacBook Air', description: '13-inch MacBook Air', price: '$1100', image: 'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-air-gold-select-201810?wid=892&hei=820&&qlt=80&.v=1603332211000' },
-    ];
 
     return (
         <main className={classes.content}>
