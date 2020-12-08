4e76e09720359a0219f7f69cb84f3519c52b5ef6
CJ Celimen <cjdcelimen@gmail.com>
2020-12-08 16:46:19 +0800

    Update initial address form

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+import CustomTextField from './CustomTextField';
 
 const AddressForm = () => {
+    const methods = useForm();
+
     return (
-        <div>
-            AddressForm
-        </div>
+        <>
+            <Typography variant='h6' gutterBottom>Shipping Address</Typography>
+            <FormProvider {...methods}>
+                <form>
+                    <Grid container spacing={3}>
+                        <CustomTextField required name='firstName' label='First name' />
+                        <CustomTextField required name='lastName' label='Last name' />
+                        <CustomTextField required name='address1' label='Address' />
+                        <CustomTextField required name='email' label='Email' />
+                        <CustomTextField required name='city' label='City' />
+                        <CustomTextField required name='zip' label='ZIP/Postal code' />
+                    </Grid>
+                </form>
+            </FormProvider>
+        </>
     )
 }
 
