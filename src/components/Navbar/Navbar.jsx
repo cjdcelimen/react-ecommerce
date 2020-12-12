2f012ffb64687b919d9d68ee8fdaaa2c6d66ac7e
CJ Celimen <cjdcelimen@gmail.com>
2020-12-12 08:04:20 +0800

    Update payment functionality with Stripe

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,8 +14,8 @@ const Navbar = ({ totalItems }) => {
             <AppBar position='fixed' className={classes.appBar} color='inherit'>
                 <Toolbar>
                     <Typography component={Link} to='/' variant='h6' className={classes.title} color='inherit'>
-                        <img src={logo} alt='Commerce.js' height='25px' className={classes.image} />
-                        cShop
+                        {/* <img src={logo} alt='Commerce.js' height='25px' className={classes.image} /> */}
+                        eCommerce
                     </Typography>
                     <div className={classes.grow} />
                     {location.pathname === '/' && (
